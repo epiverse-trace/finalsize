7ef6d7f4ef09c0783f39f74a36fa0a51f0dd9cb5
adamkucharski <adam.kucharski@lshtm.ac.uk>
2022-05-04 15:26:16 +0100

    Initial commit

---
+++ R/final_size.R
@@ -0,0 +1,54 @@
+#' Calculate final epidemic size
+#'
+#' This function calculates final epidemic size using SIR model for heterogeneously mixing population
+#' @param r0  Basic reproduction number. Default is 2
+#' @param contact_matrix  Social contact matrix. Entry mm_ij gives average number of contacts in group i reported by participants in group j
+#' @param demography_vector  Demography vector. Entry pp_i gives proportion of total population in group i (model will normalise if needed)
+#' @param prop_suscep  Proportion of each group susceptible. Null assumption is fully susceptible
+#' @keywords epidemic model
+#' @export
+#' @examples
+#' final_size()
+
+final_size<-function(r0=2,contact_matrix,demography_vector,prop_suscep=NULL){
+  
+  # Check inputs
+  if(is.null(prop_suscep)){prop_suscep <- demography_vector*0 + 1} # Assume fully susceptible if no entry
+  if(length(contact_matrix[1,])!=length(demography_vector)){stop("demography vector needs to be same size as contact matrix")}
+  if(length(demography_vector)!=length(prop_suscep)){stop("demography vector needs to be same size as susceptibility vector")}
+  pp0=as.numeric(demography_vector/sum(demography_vector))
+  
+  # Scale next generation matrix so max eigenvalue=r0
+  mm0 <- as.matrix(contact_matrix)
+  mm0 <- r0*mm0/max(Re(eigen(mm0)$values))
+  
+  # Define transmission matrix A = mm_ij*pp_j/pp_i
+  beta1 <- (mm0* prop_suscep) /pp0
+  beta2 <- t(t(beta1)*pp0)
+  
+  # Newton method for solving final size equation: A(1-x) = -log(x)
+  
+  # Define functions f and f'
+  vsize <- length(pp0)
+  f1 <- function(beta2,x){ beta2%*%(1-x) +log(x) }
+  f2 <- function(beta2,x,size){-beta2+diag(size)/x }
+
+  # Set storage vector and precision
+  iterations <- 30
+  iterate_output <- matrix(NA,nrow=iterations,ncol=vsize)
+  x0 <- 0.001*pp0 # Set starting point
+  
+  for(ii in 1:iterations){
+    if(ii==1){
+        xx=x0
+        iterate_output[ii,]=xx
+      }else{
+        dx <- solve(f2(beta2,xx,vsize), -f1(beta2,xx))
+        iterate_output[ii,] <- xx+dx
+        xx <- as.numeric(xx+dx)
+      }
+  }
+  
+  (1-iterate_output[iterations,])
+  
+}
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # finalsize
-Final size epidemic model
+
+`R` package to calculate final size for SIR epidemic in a heterogenous population.
+
+## Installation
+
+The easiest way to install the development version of `finalsize` is to use the `devtools` package:
+
+```r
+# install.packages("devtools")
+library(devtools)
+install_github("epiverse-trace/finalsize")
+library(finalsize)
+
+```
+
+## Quick start
+
+Use `final_size` to calculate the final size of an epidemic using social contact data from the `socialmixr` package. In the below example, we consider three age groups: 0-19, 20-39 and 40+, with R0 = 2. We also assume that under 20 age group is fully susceptible, whereas only 50% of individuals age 20+ are.
+
+```
+# Load socialmixr package
+
+# install.packages("socialmixr")
+library(socialmixr)
+
+# Load data from POLYMOD
+data(polymod)
+contact_data <- contact_matrix(polymod, countries = "United Kingdom", age.limits = c(0,20,40))
+
+c_matrix <-t(contact_data$matrix) # Define contact matrix (entry ij is contacts in group i reported by group j)
+d_vector <- contact_data$participants$proportion # Define proportion in each age group
+p_suscep <- c(1,0.5,0.5) # Define proportion of age group that is susceptible to infection
+
+# Run final size model
+final_size(r0=2,contact_matrix = c_matrix, demography_vector = d_vector, prop_suscep = p_suscep)
+```
+
+
+## Citation
+
+Kucharski AJ, Kwok KO, Wei VW, Cowling BJ, Read JM, Lessler J, Cummings DA, Riley S. [The contribution of social behaviour to the transmission of influenza A in a human population](http://journals.plos.org/plospathogens/article?id=10.1371/journal.ppat.1004206). PLOS Pathogens 2014;10(6):e1004206 PMID: 24968312
