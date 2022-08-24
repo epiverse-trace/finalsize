087f447c5d208ef2f0ac80f882a4b7dffd3a1e46
Pratik Gupte <pratikgupte16@gmail.com>
2022-08-24 16:04:17 +0100

    final_size code using cpp backend

---
+++ R/final_size_cpp.R
@@ -0,0 +1,51 @@
+#' Calculate final epidemic size
+#'
+#' This function calculates final epidemic size using SIR model for
+#' heterogeneously mixing population
+#' @param r0  Basic reproduction number. Default is 2.
+#' @param contact_matrix  Social contact matrix. Entry mm_ij gives average
+#' number of contacts in group i reported by participants in group j.
+#' @param demography  Demography vector. Entry pp_i gives proportion of
+#' total population in group i (model will normalise if needed)
+#' @param susceptibility  Proportion of each group susceptible. Null assumption is
+#' fully susceptible.
+#' @keywords epidemic model
+#' @export
+#'
+#' @examples
+#' # basic usage of finalsize
+#' \dontrun{
+#' final_size()
+#' }
+#'
+final_size_cpp <- function(r0 = 2, contact_matrix, demography,
+                           susceptibility = NULL) {
+  
+  # Check inputs
+  if (is.null(susceptibility)) {
+    susceptibility <- demography * 0 + 1
+  } # Assume fully susceptible if no entry
+  if (nrow(contact_matrix) != length(demography)) {
+    stop("demography vector needs to be same size as contact matrix")
+  }
+  if (length(demography) != length(susceptibility)) {
+    stop("demography vector needs to be same size as susceptibility vector")
+  }
+  pp0 <- as.numeric(demography / sum(demography))
+  
+  # Scale next generation matrix so max eigenvalue=r0
+  mm0 <- as.matrix(contact_matrix)
+  mm0 <- r0 * mm0 / max(Re(eigen(mm0)$values))
+  
+  tryCatch(
+    expr = solve_final_size_internal(
+      contact_matrix = contact_matrix,
+      demography = demography, p_susceptibility = susceptibility,
+      susceptibility = as.matrix(1)
+    ),
+    error = function(e) {
+      print(e)
+      message("final size cpp failed as expected")
+    }
+  )
+}
