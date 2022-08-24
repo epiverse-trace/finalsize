9104dc6f94b8a62e096d9827b0cfc531a497a3da
Pratik Gupte <pratikgupte16@gmail.com>
2022-08-24 16:06:33 +0100

    rcpp exports auto generated

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -11,26 +11,25 @@ Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
 Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
-// final_size_newton_cpp
-Eigen::ArrayXd final_size_newton_cpp(const Eigen::MatrixXd& contact_matrix, const Eigen::VectorXd& demography, const Eigen::VectorXd& susceptibility, const bool& adapt_step, const double& step_rate, const bool& cache_pi, const double& tolerance);
-RcppExport SEXP _finalsize_final_size_newton_cpp(SEXP contact_matrixSEXP, SEXP demographySEXP, SEXP susceptibilitySEXP, SEXP adapt_stepSEXP, SEXP step_rateSEXP, SEXP cache_piSEXP, SEXP toleranceSEXP) {
+// solve_final_size_internal
+inline Eigen::VectorXd solve_final_size_internal(const Eigen::MatrixXd& contact_matrix, const Eigen::VectorXd& demography, const Eigen::MatrixXd& p_susceptibility, const Eigen::MatrixXd& susceptibility, const bool adapt_step, const double tolerance);
+RcppExport SEXP _finalsize_solve_final_size_internal(SEXP contact_matrixSEXP, SEXP demographySEXP, SEXP p_susceptibilitySEXP, SEXP susceptibilitySEXP, SEXP adapt_stepSEXP, SEXP toleranceSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< const Eigen::MatrixXd& >::type contact_matrix(contact_matrixSEXP);
     Rcpp::traits::input_parameter< const Eigen::VectorXd& >::type demography(demographySEXP);
-    Rcpp::traits::input_parameter< const Eigen::VectorXd& >::type susceptibility(susceptibilitySEXP);
-    Rcpp::traits::input_parameter< const bool& >::type adapt_step(adapt_stepSEXP);
-    Rcpp::traits::input_parameter< const double& >::type step_rate(step_rateSEXP);
-    Rcpp::traits::input_parameter< const bool& >::type cache_pi(cache_piSEXP);
-    Rcpp::traits::input_parameter< const double& >::type tolerance(toleranceSEXP);
-    rcpp_result_gen = Rcpp::wrap(final_size_newton_cpp(contact_matrix, demography, susceptibility, adapt_step, step_rate, cache_pi, tolerance));
+    Rcpp::traits::input_parameter< const Eigen::MatrixXd& >::type p_susceptibility(p_susceptibilitySEXP);
+    Rcpp::traits::input_parameter< const Eigen::MatrixXd& >::type susceptibility(susceptibilitySEXP);
+    Rcpp::traits::input_parameter< const bool >::type adapt_step(adapt_stepSEXP);
+    Rcpp::traits::input_parameter< const double >::type tolerance(toleranceSEXP);
+    rcpp_result_gen = Rcpp::wrap(solve_final_size_internal(contact_matrix, demography, p_susceptibility, susceptibility, adapt_step, tolerance));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_finalsize_final_size_newton_cpp", (DL_FUNC) &_finalsize_final_size_newton_cpp, 7},
+    {"_finalsize_solve_final_size_internal", (DL_FUNC) &_finalsize_solve_final_size_internal, 6},
     {NULL, NULL, 0}
 };
 
