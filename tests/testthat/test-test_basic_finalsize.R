fc9020192893770ec9345dfca38e323910fbaf04
Pratik Gupte <pratikgupte16@gmail.com>
2022-09-02 10:51:13 +0100

    style test file

--- tests/testthat/test-test_basic_finalsize.R
+++ tests/testthat/test-test_basic_finalsize.R
@@ -8,14 +8,14 @@ test_that("Check basic final size calculation works", {
   c_matrix <- t(contact_data$matrix)
   d_vector <- contact_data$participants$proportion
   p_suscep <- c(1, 1, 1)
-  
+
   epi_final_size <- final_size(
     r0 = 2,
     contact_matrix = c_matrix,
     demography_vector = d_vector,
     prop_suscep = p_suscep
   )
-  
+
   # Run final size model
   testthat::expect_identical(
     length(p_suscep), length(epi_final_size)
