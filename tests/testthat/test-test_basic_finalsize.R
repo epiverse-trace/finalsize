654b47e13f76c12077360980baac028e5eea09c4
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-06 14:37:53 +0100

    Check content error/message, add error test for prop_initial_infect

--- tests/testthat/test-test_basic_finalsize.R
+++ tests/testthat/test-test_basic_finalsize.R
@@ -43,7 +43,8 @@ test_that("Check failure for unequal contact matrix and demography", {
       demography_vector = d_vector,
       prop_initial_infected = p_initial_infections,
       prop_suscep = p_suscep
-    )
+    ),
+    regexp = "(demography vector)*(same size)*(contact matrix)"
   )
 })
 
@@ -55,8 +56,8 @@ test_that("Check failure for unequal demography and susceptibility vectors", {
     age.limits = c(0, 20, 40)
   )
   c_matrix <- t(contact_data$matrix)
-  d_vector <- c(0.5, 0.5) # manually created
-  p_suscep <- c(1, 1)
+  d_vector <- c(0.5, 0.5, 0.5) # manually created
+  p_suscep <- c(1, 1, 1, 1) # one extra susceptibility value
   p_initial_infections <- c(0.0015)
 
   testthat::expect_error(
@@ -66,11 +67,12 @@ test_that("Check failure for unequal demography and susceptibility vectors", {
       demography_vector = d_vector,
       prop_initial_infected = p_initial_infections,
       prop_suscep = p_suscep
-    )
+    ),
+    regexp = "(demography vector)*(same size)*(susceptibility vector)"
   )
 })
 
-test_that("Check message for multiple susceptibilities", {
+test_that("Check message for multiple initial infection proportions", {
   polymod <- socialmixr::polymod
   contact_data <- socialmixr::contact_matrix(
     polymod,
@@ -89,6 +91,31 @@ test_that("Check message for multiple susceptibilities", {
       demography_vector = d_vector,
       prop_initial_infected = p_initial_infections,
       prop_suscep = p_suscep
-    )
+    ),
+    regexp = "(different)*(prop_initial_infected)*(age group)"
+  )
+})
+
+test_that("Check error for unequal proportions initial infections", {
+  polymod <- socialmixr::polymod
+  contact_data <- socialmixr::contact_matrix(
+    polymod,
+    countries = "United Kingdom",
+    age.limits = c(0, 20, 40)
+  )
+  c_matrix <- t(contact_data$matrix)
+  d_vector <- contact_data$participants$proportion # manually created
+  p_suscep <- c(1, 1, 1)
+  p_initial_infections <- c(0.1, 0.5, 0.5, 0.1)
+
+  testthat::expect_error(
+    final_size(
+      r0 = 2,
+      contact_matrix = c_matrix,
+      demography_vector = d_vector,
+      prop_initial_infected = p_initial_infections,
+      prop_suscep = p_suscep
+    ),
+    regexp = "(vector)*(prop_initial_infected)*(same size)*(demography vector)"
   )
 })
