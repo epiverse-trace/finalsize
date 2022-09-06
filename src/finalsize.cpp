e68f8864949b09f3aff340ed5c961724cd852375
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-06 09:25:25 +0100

    WIP Rcpp implementation fails on matrix division

--- src/finalsize.cpp
+++ src/finalsize.cpp
@@ -11,16 +11,24 @@
 //' C++ backend to calculate final epidemic size.
 //'
 // [[Rcpp::export]]
-Rcpp::NumericVector final_size_cpp (const double &r0,
+Eigen::ArrayXd final_size_cpp (const double &r0,
     const Eigen::MatrixXd &contact_matrix,
-    const Eigen::VectorXd &demography_vector
+    const Eigen::VectorXd &demography_vector,
+    const Eigen::VectorXd &prop_suscep
 ) {
     // scale demography vector
     Eigen::VectorXd pp0 = demography_vector / (demography_vector.sum());
 
     // largest real eigenvalue of the contact matrix
-    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es(contact_matrix, false);
-    double eigv_max = es.eigenvalues().maxCoeff();
+    Eigen::EigenSolver<Eigen::MatrixXd> es(contact_matrix, false);
+    Eigen::MatrixXd eig_vals = es.eigenvalues().real();
+    double eig_val_max = eig_vals.maxCoeff();
+    // scale the next generation matrix for max eigenvalue = r0
+    Eigen::MatrixXd mm0 = r0 * (contact_matrix / eig_val_max);
+    
+    // define transmission matrix A = mm_{ij} * pp_{j} / pp_{i}
+    Eigen::Map<Eigen::MatrixXd> pp1(pp0.data(), pp0.size(), 1);
+    Eigen::ArrayXd beta1 = (mm0 * prop_suscep).array() / pp1.array();
 
-    return Rcpp::NumericVector::create(eigv_max);
-}
+    return beta1;
+}   
