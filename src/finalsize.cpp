1a2ccbede4a024ad49947eae3600a117a44e9241
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-06 12:40:48 +0100

    WIP implementing Rcpp version of final_size; prep transmission matrix

--- src/finalsize.cpp
+++ src/finalsize.cpp
@@ -21,10 +21,10 @@ inline Eigen::MatrixXd f1 (Eigen::MatrixXd &beta2, const Eigen::VectorXd &x) {
 //' C++ backend to calculate final epidemic size.
 //'
 // [[Rcpp::export]]
-Eigen::ArrayXd final_size_cpp (const double &r0,
+Rcpp::List final_size_cpp (const double &r0,
     const Eigen::MatrixXd &contact_matrix,
     const Eigen::VectorXd &demography_vector,
-    const Eigen::VectorXd &prop_suscep
+    const double &prop_suscep
 ) {
     // scale demography vector
     Eigen::VectorXd pp0 = demography_vector / (demography_vector.sum());
@@ -37,8 +37,33 @@ Eigen::ArrayXd final_size_cpp (const double &r0,
     Eigen::MatrixXd mm0 = r0 * (contact_matrix / eig_val_max);
     
     // define transmission matrix A = mm_{ij} * pp_{j} / pp_{i}
-    Eigen::Map<Eigen::MatrixXd> pp1(pp0.data(), pp0.size(), 1);
-    Eigen::ArrayXd beta1 = (mm0 * prop_suscep).array() / pp1.array();
+    Eigen::MatrixXd beta1 = (mm0 * prop_suscep).array();
+    // rowwise division by age group proportion
+    // this is vectorised in final_size.R
+    // with demography vector recycled over columns; i.e., age 1 * row 1 etc.
+    for (size_t i = 0; i < beta1.rows(); i++)
+        beta1.row(i) = beta1.row(i) / pp0[i];
 
-    return beta1;
-}   
+    Eigen::MatrixXd beta2 = beta1.transpose();
+    // rowwise multiplication with corresponding age group proportion
+    for (size_t i = 0; i < beta2.rows(); i++)
+        beta2.row(i) = beta2.row(i) * pp0[i];
+    
+    beta2 = (beta2.array().transpose()).matrix(); // could be more concise?
+
+    // Newton solver for final size equation A(1-x) = -log(x)
+    // get number of age groups
+    const size_t v_size = pp0.size();
+
+    // define functions f1 and f2
+    
+    return Rcpp::List::create(
+        pp0,
+        mm0,
+        prop_suscep,
+        beta1,
+        beta2,
+        v_size,
+        f1 (beta2, pp0)
+    );
+}
