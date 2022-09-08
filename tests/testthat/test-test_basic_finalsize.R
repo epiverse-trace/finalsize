563184e5f74448eb7420795320ac0648076c2364
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-08 16:10:24 +0100

    Add tests for r0

--- tests/testthat/test-test_basic_finalsize.R
+++ tests/testthat/test-test_basic_finalsize.R
@@ -24,6 +24,49 @@ test_that("Check basic final size calculation works", {
   )
 })
 
+test_that("Check final size r0 inputs", {
+  c_matrix <- matrix(0.5, 2, 2)
+  d_vector <- rep(0.5, 2)
+  p_suscep <- c(1)
+  p_initial_infections <- 0.0015
+
+  # expect error for negative r0
+  testthat::expect_error(
+    final_size(
+      r0 = -1.0,
+      contact_matrix = c_matrix,
+      demography_vector = d_vector,
+      prop_initial_infected = p_initial_infections,
+      prop_suscep = p_suscep
+    ),
+    regexp = "(Element 1 is not >= 1)"
+  )
+
+  # expect error for r0 < 1
+  testthat::expect_error(
+    final_size(
+      r0 = 0.99,
+      contact_matrix = c_matrix,
+      demography_vector = d_vector,
+      prop_initial_infected = p_initial_infections,
+      prop_suscep = p_suscep
+    ),
+    regexp = "(Element 1 is not >= 1)"
+  )
+
+  # expect error for infinite r0
+  testthat::expect_error(
+    final_size(
+      r0 = Inf,
+      contact_matrix = c_matrix,
+      demography_vector = d_vector,
+      prop_initial_infected = p_initial_infections,
+      prop_suscep = p_suscep
+    ),
+    regexp = "(Must be finite)"
+  )
+})
+
 test_that("Check failure for unequal contact matrix and demography", {
   polymod <- socialmixr::polymod
   contact_data <- socialmixr::contact_matrix(
