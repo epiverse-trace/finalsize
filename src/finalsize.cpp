441e574d12bf94473cedb51053c00cca0e3f1cde
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-09 16:42:25 +0100

    WIP remove internal funs from src file

--- src/finalsize.cpp
+++ src/finalsize.cpp
@@ -10,41 +10,6 @@
 //
 // [[Rcpp::depends(RcppEigen)]]
 
-/// function f1 defined in final_size.R
-// f1 <- function(beta2, x) {
-//     beta2 %*% (1 - x) + log(x)
-//   }
-inline Eigen::MatrixXd f1 (Eigen::MatrixXd beta2, const Eigen::VectorXd x) {
-    
-    Eigen::MatrixXd x_ = (beta2 * ((Eigen::VectorXd::Ones(x.size()) - x))) + 
-        ((x.array().log()).matrix());
-    
-    return x_;
-}
-
-/// function f2 defined in final_size.R
-// f2 <- function(beta2, x, size) {
-//     -beta2 + diag(size) / x
-//   }
-inline Eigen::MatrixXd f2 (Eigen::MatrixXd beta2, const Eigen::VectorXd x,
-    const size_t &size
-) {
-    // make diagonal matrix of dims [size, size]
-    // size should be the number of age groups
-    Eigen::VectorXd v = Eigen::VectorXd::Ones(size);
-    Eigen::MatrixXd diag_size;
-    diag_size.resize(size, size);
-    diag_size.fill(0.0);
-    diag_size.diagonal() = v;
-
-    // divide diagonal matrix of 1s by x
-    // x is the current proportion infected per age group
-    for (size_t i = 0; i < diag_size.rows(); i++)
-        diag_size.row(i) = diag_size.row(i) / x[i];
-    
-    return -beta2 + diag_size;
-}
-
 //' @title Calculate final epidemic size with RcppEigen backend
 //'
 //' @description This function calculates final epidemic size using SIR model 
