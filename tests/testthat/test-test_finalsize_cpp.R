dfde8afa3b78c01ef0ed42193caaf0648fa97fa7
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-09 15:39:07 +0100

    WIP add test for polymod data for final_size_cpp

--- tests/testthat/test-test_finalsize_cpp.R
+++ tests/testthat/test-test_finalsize_cpp.R
@@ -30,6 +30,8 @@ test_that("Check final_size_cpp calculation works", {
     all(epi_final_size >= p_initial_infections)
   )
 })
+
+test_that("Check final_size_cpp with Polymod data", {
   polymod <- socialmixr::polymod
   contact_data <- socialmixr::contact_matrix(
     polymod,
@@ -38,15 +40,11 @@ test_that("Check final_size_cpp calculation works", {
   )
   c_matrix <- t(contact_data$matrix)
   d_vector <- contact_data$participants$proportion
-  p_suscep <- c(1)
+  p_suscep <- c(1, 1, 1)
   p_initial_infections <- 0.0015
 
-  for(i in seq(ncol(c_matrix))) {
-    c_matrix[, i] = c_matrix[, i] / d_vector[i]
-  }
-
-  epi_final_size <- final_size_cpp(
-    r0 = 1.3,
+  epi_final_size <- final_size(
+    r0 = 2,
     contact_matrix = c_matrix,
     demography_vector = d_vector,
     prop_initial_infected = p_initial_infections,
@@ -55,6 +53,23 @@ test_that("Check final_size_cpp calculation works", {
 
   # Run final size model
   testthat::expect_identical(
+    length(p_suscep), length(epi_final_size)
+  )
+  # basic tests for output
+  testthat::expect_identical(
     length(d_vector), length(epi_final_size)
+  )
+  # test that output is a numeric vector
+  testthat::expect_vector(
+    epi_final_size,
+    ptype = numeric()
+  )
+  # test that the final size is plausible
+  testthat::expect_true(
+    all(epi_final_size <= 1.0)
+  )
+  # expect that final size is greater than p_initial_infections
+  testthat::expect_true(
+    all(epi_final_size >= p_initial_infections)
   )
 })
