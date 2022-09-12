2a164f4985907b48e9d8b7bbd0ade04fcaed9fbf
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-12 16:47:12 +0100

    Style files with clang-format

--- src/test-internal_funs.cpp
+++ src/test-internal_funs.cpp
@@ -9,22 +9,22 @@
 
 // All test files should include the <testthat.h>
 // header file.
-#include <testthat.h>
 #include <Rcpp.h>
 #include <RcppEigen.h>
+#include <testthat.h>
+
 #include "finalsize.h"
 
 // check that function f1 returns output of expected dimensions
 context("Fn f1 return is expected length") {
-  
   test_that("Function f1 returns a 1 row matrix") {
     // set up test inputs
     int n_groups = 3;
     Eigen::MatrixXd beta2;
     beta2.resize(n_groups, n_groups);
-    beta2.fill(0.2); // fill with an arbitrary number
+    beta2.fill(0.2);  // fill with an arbitrary number
 
-    Eigen::Vector3d x(0.2, 0.2, 0.2); // same length as n_groups
+    Eigen::Vector3d x(0.2, 0.2, 0.2);  // same length as n_groups
     CATCH_REQUIRE(f1(beta2, x).size() == n_groups);
     CATCH_REQUIRE(f1(beta2, x).rows() == n_groups);
     CATCH_REQUIRE(f1(beta2, x).cols() == 1);
@@ -33,15 +33,14 @@ context("Fn f1 return is expected length") {
 
 // check that function f2 returns output of expected dimensions
 context("Fn f2 return is expected size") {
-  
   test_that("Function f1 returns a 1 row matrix") {
     // set up test inputs
     int n_groups = 3;
     Eigen::MatrixXd beta2;
     beta2.resize(n_groups, n_groups);
-    beta2.fill(0.2); // fill with an arbitrary number
+    beta2.fill(0.2);  // fill with an arbitrary number
 
-    Eigen::Vector3d x(0.2, 0.2, 0.2); // same length as n_groups
+    Eigen::Vector3d x(0.2, 0.2, 0.2);  // same length as n_groups
 
     CATCH_REQUIRE(f2(beta2, x, n_groups).size() == beta2.size());
     CATCH_REQUIRE(f2(beta2, x, n_groups).rows() == n_groups);
