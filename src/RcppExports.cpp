d1b510af1dc37846c655e424862544eca49c3235
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-12 12:14:02 +0100

    Update Rcpp exports

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -12,7 +12,7 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 // final_size_cpp
-Eigen::VectorXd final_size_cpp(const double& r0, const Eigen::MatrixXd& contact_matrix, const Eigen::VectorXd& demography_vector, const Eigen::VectorXd& prop_initial_infected, const double& prop_suscep, const int iterations);
+Eigen::VectorXd final_size_cpp(const double& r0, const Eigen::MatrixXd& contact_matrix, const Eigen::VectorXd& demography_vector, const Eigen::VectorXd& prop_initial_infected, Eigen::VectorXd& prop_suscep, const int iterations);
 RcppExport SEXP _finalsize_final_size_cpp(SEXP r0SEXP, SEXP contact_matrixSEXP, SEXP demography_vectorSEXP, SEXP prop_initial_infectedSEXP, SEXP prop_suscepSEXP, SEXP iterationsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
@@ -21,7 +21,7 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< const Eigen::MatrixXd& >::type contact_matrix(contact_matrixSEXP);
     Rcpp::traits::input_parameter< const Eigen::VectorXd& >::type demography_vector(demography_vectorSEXP);
     Rcpp::traits::input_parameter< const Eigen::VectorXd& >::type prop_initial_infected(prop_initial_infectedSEXP);
-    Rcpp::traits::input_parameter< const double& >::type prop_suscep(prop_suscepSEXP);
+    Rcpp::traits::input_parameter< Eigen::VectorXd& >::type prop_suscep(prop_suscepSEXP);
     Rcpp::traits::input_parameter< const int >::type iterations(iterationsSEXP);
     rcpp_result_gen = Rcpp::wrap(final_size_cpp(r0, contact_matrix, demography_vector, prop_initial_infected, prop_suscep, iterations));
     return rcpp_result_gen;
