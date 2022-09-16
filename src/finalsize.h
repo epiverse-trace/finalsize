ad25f596687a3c1aa83f93460e1aa7740b8663ee
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-16 12:53:28 +0100

    Add epi_spread fun from @BlackEdder snippet
    Correct struct member in return

--- src/finalsize.h
+++ src/finalsize.h
@@ -39,4 +39,42 @@ inline Eigen::MatrixXd f2(Eigen::MatrixXd beta2, const Eigen::VectorXd x,
   return -beta2 + diag_size;
 }
 
+/// A function for epidemic spread with susceptibility groups
+// taken from Edwin van Leeuwen at https://gitlab.com/epidemics-r/code_snippets/feature/newton_solver/include/finalsize.hpp
+Rcpp::List epi_spread(const Eigen::MatrixXd &contact_matrix,
+                      const Eigen::VectorXd &demography_vector,
+                      const Eigen::MatrixXd &p_susceptibility, // risk groups
+                      const Eigen::MatrixXd &susceptibility // susc of risk grp?
+) {
+
+  // check dimensions
+
+  // count number of risk groups
+  auto n_susc_groups = p_susceptibility.cols();
+  Eigen::MatrixXd p_susceptibility_ = Eigen::MatrixXd::Ones(p_susceptibility.size(), 1);
+  
+  // make single column matrix from prop_suscep data,
+  // prop_suscep is the prob(suscep) per demography group
+  Eigen::MatrixXd psusc = p_susceptibility;
+  Eigen::Map<Eigen::MatrixXd> lps(psusc.data(), psusc.size(), 1);
+
+  // replicate demography vector by the number of risk groups
+  // and multiply it by the prop_suscep values
+  Eigen::ArrayXd demography_vector_ = demography_vector.replicate(n_susc_groups, 1).array() * lps.array();
+
+  Eigen::MatrixXd contact_matrix_ = contact_matrix.replicate(n_susc_groups, n_susc_groups);
+
+  // unroll the risk level matrix
+  Eigen::MatrixXd susc = susceptibility;
+  Eigen::Map<Eigen::MatrixXd> rm(susc.data(), susc.size(), 1);
+
+  epi_spread_data tmp_data;
+  tmp_data.contact_matrix = contact_matrix_;
+  tmp_data.demography_vector = demography_vector_;
+  tmp_data.p_susceptibility = p_susceptibility_;
+  tmp_data.susceptibility = rm;
+
+  return tmp_data;
+}
+
 #endif
