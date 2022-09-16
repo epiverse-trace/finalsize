fe1c725d85f6089a6ab4823d541579701d545d43
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-16 12:56:14 +0100

    Add checks for finalsize_by_grps incl. error messages

--- tests/testthat/test-finalsize_by_group.R
+++ tests/testthat/test-finalsize_by_group.R
@@ -104,5 +104,83 @@ test_that("Check final size calculation is correct in complex case", {
     epi_outcome, "double"
   )
 
+  testthat::expect_equal(
+    length(epi_outcome), length(demography_vector)
+  )
+
   # TO DO: ADD CHECK FOR CORRECT ANSWER
+})
+
+# check for errors and messages
+test_that("Check for errors and messages", {
+  # checking epi spread function from finalsize
+  polymod <- socialmixr::polymod
+  contact_data <- socialmixr::contact_matrix(
+    polymod,
+    countries = "United Kingdom",
+    age.limits = c(0, 20, 40),
+    symmetric = TRUE
+  )
+  p_susceptibility <- matrix(1, ncol = 1, nrow = 3)
+  susceptibility <- matrix(1, ncol = 1, 3)
+
+  demography_vector <- contact_data$demography$proportion
+
+  # 'wrong' demography vector
+  demography_vector <- c(demography_vector, 100)
+
+  contact_matrix <- matrix(contact_data$matrix, ncol = 3)
+
+  # expect error on demography vector and contact matrix
+  testthat::expect_error(
+    final_size_grps_cpp(
+      contact_matrix = contact_matrix,
+      demography_vector = demography_vector,
+      p_susceptibility = p_susceptibility,
+      susceptibility = susceptibility
+    ),
+    regexp = "Error: contact matrix must have as many rows as demography groups"
+  )
+
+  demography_vector <- demography_vector[-length(demography_vector)]
+  p_susceptibility <- matrix(1, ncol = 1, nrow = 4)
+
+  # expect error on demography vector and p_susceptibility
+  testthat::expect_error(
+    final_size_grps_cpp(
+      contact_matrix = contact_matrix,
+      demography_vector = demography_vector,
+      p_susceptibility = p_susceptibility,
+      susceptibility = susceptibility
+    ),
+    regexp = "Error: p_susceptibility must have as many rows as demography"
+  )
+
+  p_susceptibility <- matrix(1, ncol = 1, nrow = 3)
+  susceptibility <- matrix(1, ncol = 1, nrow = 4)
+
+  # expect error on demography vector and susceptibility
+  testthat::expect_error(
+    final_size_grps_cpp(
+      contact_matrix = contact_matrix,
+      demography_vector = demography_vector,
+      p_susceptibility = p_susceptibility,
+      susceptibility = susceptibility
+    ),
+    regexp = "Error: susceptibility must have as many rows as demography groups"
+  )
+
+  p_susceptibility <- matrix(1, ncol = 1, nrow = 3)
+  susceptibility <- matrix(1, ncol = 2, nrow = 3)
+
+  # expect error on p_susceptibility and susceptibility
+  testthat::expect_error(
+    final_size_grps_cpp(
+      contact_matrix = contact_matrix,
+      demography_vector = demography_vector,
+      p_susceptibility = p_susceptibility,
+      susceptibility = susceptibility
+    ),
+    regexp = "Error: p_susceptibility and susceptibility must be matrices of the same dims"
+  )
 })
