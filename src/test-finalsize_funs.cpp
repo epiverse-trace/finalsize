9f3096a2edcb25f4ee49fd08126eac7086bea534
Pratik Gupte <pratikgupte16@gmail.com>
2022-09-27 16:37:09 +0100

    Better tests for final size by susceptibility groups (#43)
    * WIP finalsize_grps_cpp tests: test Newton solver
    
    * WIP finalsize_grps_cpp tests: Test internal solve fun
    
    * Check finalsize_grps_cpp with r0 = 1.3 (passes)
    
    * Check finalsize_grps with r0 = 2.0 (fails)
    
    * WIP formatting for R tests
    
    * Test Cpp solve_by_susc with r0 = 2.0 (fails)
    
    * Return ArrayXd
    
    * Rcpp export return ArrayXd
    
    * Test Newton solver with r0 = 2.0 (fails)

--- src/test-finalsize_funs.cpp
+++ src/test-finalsize_funs.cpp
@@ -117,6 +117,44 @@ context("Newton solver works for single risk group") {
   }
 }
 
+/// check Newton solver returns a final size > 0 and < 1 for ONE risk grp
+context("Newton solver works for single risk group, r0 = 2") {
+
+  double r0 = 2.0;
+  // make some function arguments
+  Eigen::MatrixXd contact_matrix(2, 2);
+  contact_matrix << 1.0, 0.5, 0.5, 1.0;
+  Eigen::VectorXd demography_vector(2);
+  demography_vector << 1.0, 1.0;
+
+  // make p_susceptibility with one (1) risk group
+  Eigen::MatrixXd p_susceptibility(2, 1);  // n rows per demo grp, 2 col risk
+  p_susceptibility << 0.25, 0.75;
+
+  Eigen::MatrixXd susceptibility = p_susceptibility;
+  susceptibility.fill(1.0);  // all groups have same susceptibility?
+
+  epi_spread_data tmp_data = epi_spread(contact_matrix, demography_vector,
+                                        p_susceptibility, susceptibility);
+
+  Eigen::ArrayXd final_size_temp = solve_final_size_newton(
+      r0 * contact_matrix, demography_vector, p_susceptibility, susceptibility);
+
+  test_that("Fn solve_final_size_newton returns same size as demography, r0 = 2") {
+    CATCH_CHECK(final_size_temp.size() == demography_vector.size());
+  }
+  test_that("Fn solve_final_size_newton values LTE 1.0, r0 = 2") {
+    for (size_t i = 0; i < final_size_temp.size(); i++) {
+      CATCH_CHECK(final_size_temp[i] <= 1.0);
+    }
+  }
+  test_that("Fn solve_final_size_newton values GT 0, r0 = 2") {
+    for (size_t i = 0; i < final_size_temp.size(); i++) {
+      CATCH_CHECK(final_size_temp[i] > 0.0);
+    }
+  }
+}
+
 /// check Newton solver returns a final size > 0 and < 1 for MULT risk grps
 context("Newton solver works for multiple risk groups") {
   // make some function arguments
@@ -153,7 +191,6 @@ context("Newton solver works for multiple risk groups") {
   }
 }
 
-
 /// check Newton solver returns a final size > 0 and < 1 for identical contacts
 context("Newton solver works for identical contact matrix") {
   // make some function arguments
@@ -165,6 +202,42 @@ context("Newton solver works for identical contact matrix") {
   // make p_susceptibility with one (1) risk group
   Eigen::MatrixXd p_susceptibility(2, 2);  // n rows per demo grp, 2 col risk
   p_susceptibility << 0.25, 0.75, 0.25, 0.75;
+
+  Eigen::MatrixXd susceptibility = p_susceptibility;
+  susceptibility.fill(1.0);  // all groups have same susceptibility?
+
+  epi_spread_data tmp_data = epi_spread(contact_matrix, demography_vector,
+                                        p_susceptibility, susceptibility);
+
+  Eigen::ArrayXd final_size_temp = solve_final_size_newton(
+      contact_matrix, demography_vector, p_susceptibility, susceptibility);
+
+  test_that("Fn solve_final_size_newton returns same size as demography") {
+    CATCH_CHECK(final_size_temp.size() == demography_vector.size());
+  }
+  test_that("Fn solve_final_size_newton values LTE 1.0") {
+    for (size_t i = 0; i < final_size_temp.size(); i++) {
+      CATCH_CHECK(final_size_temp[i] <= 1.0);
+    }
+  }
+  test_that("Fn solve_final_size_newton values GT 0") {
+    for (size_t i = 0; i < final_size_temp.size(); i++) {
+      CATCH_CHECK(final_size_temp[i] > 0.0);
+    }
+  }
+}
+
+/// check Newton solver for identical contact matrix with single risk grp
+context("Newton solver works for identical contact matrix with single p_susc") {
+  // make some function arguments
+  Eigen::MatrixXd contact_matrix(2, 2);
+  contact_matrix.fill(0.5);
+  Eigen::VectorXd demography_vector(2);
+  demography_vector << 1.0, 1.0;
+
+  // make p_susceptibility with one (1) risk group
+  Eigen::MatrixXd p_susceptibility(2, 1);  // n rows per demo grp, 2 col risk
+  p_susceptibility << 1.0, 1.0;
 
   Eigen::MatrixXd susceptibility = p_susceptibility;
   susceptibility.fill(1.0);  // all groups have same susceptibility?
@@ -218,8 +291,7 @@ context("Solving by susceptibility groups works") {
 
   test_that("Fn solve_by_susc_grps values LTE 1.0") {
     for (size_t i = 0; i < final_size.size(); i++) {
-      for (size_t j = 0; j < final_size.cols(); j++)
-      {
+      for (size_t j = 0; j < final_size.cols(); j++) {
         CATCH_CHECK(final_size.coeff(i, j) <= 1.0);
       }
     }
