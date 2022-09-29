fc9da5c0f30f36de408e0c85dcf9d9a12827a50b
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-09-29 15:04:18 +0100

    Test for epi_spread, WIP #46, #47

---
+++ tests/testthat/test-epi_spread.R
@@ -0,0 +1,60 @@
+# basic test that solver returns numerics within range
+test_that("Epi spread function works", {
+  # prepare some data for the solver
+  r0 <- 1.3 # not necessary
+  contact_matrix <- matrix(r0 / 200.0, 2, 2)
+  demography_vector <- rep(100.0, 2)
+  demography_vector <- demography_vector / sum(demography_vector)
+
+  # p_susceptibility and susceptibility
+  n_risk_grps <- 3
+  psusc <- matrix(1, nrow = 2, ncol = n_risk_grps)
+  psusc <- t(apply(psusc, 1, \(x) x / sum(x))) # rows sum to 1.0
+  susc <- psusc
+
+  epi_spread_data <- epi_spread(
+    contact_matrix = contact_matrix,
+    demography_vector = demography_vector,
+    p_susceptibility = psusc,
+    susceptibility = susc
+  )
+
+  # check names in epi_spread_data
+  expected_names <- c(
+    "contact_matrix", "demography_vector", "p_susceptibility_",
+    "susceptibility"
+  )
+  expect_equal(
+    setdiff(
+      names(epi_spread_data), expected_names
+    ), character()
+  )
+
+  # check that solver returns correct types
+  expect_true(
+    is.matrix(epi_spread_data[["contact_matrix"]])
+  )
+  expect_vector(
+    epi_spread_data[["demography_vector"]],
+    ptype = numeric()
+  )
+  expect_true(
+    is.matrix(epi_spread_data[["p_susceptibility_"]])
+  )
+  expect_equal(
+    ncol(epi_spread_data[["p_susceptibility_"]]),
+    1
+  )
+  expect_equal(
+    nrow(epi_spread_data[["p_susceptibility_"]]),
+    prod(dim(psusc))
+  )
+  expect_equal(
+    ncol(epi_spread_data[["susceptibility"]]),
+    1
+  )
+  expect_equal(
+    nrow(epi_spread_data[["susceptibility"]]),
+    prod(dim(susc))
+  )
+})
