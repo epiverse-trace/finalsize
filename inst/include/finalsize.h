741ce692f002f91395ea68249e3225d2b220da7d
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-02-16 13:39:08 +0000

    Edits to package header

--- inst/include/finalsize.h
+++ inst/include/finalsize.h
@@ -1,13 +1,18 @@
+// Copyright 2023 'finalsize' authors. See repository licence in LICENSE.md.
 // Written manually to allow header export
 // copied from https://github.com/r-pkg-examples/rcpp-shared-cpp-functions
 
-#ifndef finalsize_finalsize_H_GEN_
-#define finalsize_finalsize_H_GEN_
+#ifndef finalsize_finalsize_H_
+#define finalsize_finalsize_H_
 
-// Include the Rcpp Header
+// [[Rcpp::plugins(cpp14)]]
+// [[Rcpp::depends(RcppEigen)]]
+
+// Include the Rcpp Header and RcppEigen
 #include <Rcpp.h>
+#include <RcppEigen.h>
 
-#include "iterative_solver.h"
+#include "iterative_solver.h"  // paths must be relative to this file
 #include "newton_solver.h"
 
-#endif  // finalsize_finalsize_H_GEN_
+#endif  // finalsize_finalsize_H_
