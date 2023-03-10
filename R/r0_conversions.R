0dd485fb68cccb5efbacbf00ca3e511029d05d38
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-03-10 16:39:39 +0000

    Allow susceptibility groups in R0-lambda conversion, WIP #27

--- R/r0_conversions.R
+++ R/r0_conversions.R
@@ -1,12 +1,14 @@
 #' @title Calculate \eqn{R_0} from transmission rate (\eqn{\lambda})
 #'
-#' @description Uses the transmission rate (\eqn{\lambda}), contact matrix
-#' (\eqn{c}), population (\eqn{N}), and infectious period (\eqn{\gamma}) to
-#' calculate the R0 using the following equation.
-#' \deqn{\lambda max(EV(C)) \gamma}
+#' @description Uses the transmission rate (\eqn{\lambda}), social contacts
+#' matrix (\eqn{c}), demography (\eqn{N}), the distribution \eqn{P} of each
+#' demographic group \eqn{i} into susceptibility groups \eqn{S}, and the
+#' infectious period (\eqn{\gamma}) to calculate the \eqn{R0} using the
+#' following equation.
+#' \deqn{R_0 = \text{Max}(EV(C)) \times \lambda \gamma}
 #' where \eqn{EV(C)} denotes the eigenvalues of the matrix \eqn{C} which is
-#' calculated from the contact matrix and the population
-#' (\eqn{C[i,j] = c[i,j] N[j]}).
+#' calculated from the social contacts matrix scaled by the number of
+#' individuals in each demographic and susceptibility group in the population.
 #'
 #' @param lambda The transmission rate of the disease, also called the 'force of
 #'  infection' (\eqn{\lambda}). This is different from the effective
@@ -15,6 +17,12 @@
 #' number of contacts in group \eqn{i} reported by participants in group \eqn{j}
 #' @param demography_vector Demography vector. Entry \eqn{v_{i}} gives
 #' proportion of total population in group \eqn{i}.
+#' @param susceptibility A matrix giving the susceptibility of individuals in
+#' demographic group \eqn{i} and risk group \eqn{k}.
+#' @param p_susceptibility A matrix giving the probability that an individual
+#' in demography group \eqn{i} is in risk (or susceptibility) group \eqn{k}.
+#' Each row represents the overall distribution of individuals in demographic
+#' group \eqn{i} across risk groups, and each row *must sum to 1.0*.
 #' @param infectious_period Duration of the infectious period in days.
 #' Default value is 1.8 days.
 #' @export
@@ -30,12 +38,30 @@
 #'
 #' # define infectious period of 5 days
 #' infectious_period <- 5
+#' # define the number of age and susceptibility groups
+#' n_demo_grps <- length(demography_vector)
+#' n_risk_grps <- 3
+#'
+#' # In this example, all risk groups from all age groups are fully
+#' # susceptible
+#' susceptibility <- matrix(
+#'   data = 1, nrow = n_demo_grps, ncol = n_risk_grps
+#' )
+#'
+#' p_susceptibility <- matrix(
+#'   data = 1, nrow = n_demo_grps, ncol = n_risk_grps
+#' )
+#' # p_susceptibility rows must sum to 1.0
+#' p_susceptibility <- p_susceptibility / rowSums(p_susceptibility)
 #'
 #' lambda_to_r0(
-#'   lambda, contact_matrix, demography_vector, infectious_period
+#'   lambda, contact_matrix, demography_vector,
+#'   susceptibility, p_susceptibility,
+#'   infectious_period
 #' )
 #'
 lambda_to_r0 <- function(lambda, contact_matrix, demography_vector,
+                         susceptibility, p_susceptibility,
                          infectious_period = 1.8) {
   # input checking
   checkmate::assert_number(lambda, lower = 0, finite = TRUE)
@@ -55,31 +81,55 @@ lambda_to_r0 <- function(lambda, contact_matrix, demography_vector,
       (nrow(contact_matrix) == length(demography_vector))
   )
 
-  # multiply each column of the contact matrix with the corresponding
-  # demography vector element
-  cm <- t(t(contact_matrix) * demography_vector)
+  # count risk groups
+  n_susc_groups <- ncol(p_susceptibility)
+  # make lps, a vector of all p_susc values
+  lps <- as.vector(p_susceptibility)
+
+  # replicate the demography vector and multiply by p_susceptibility
+  demography_vector_spread <- rep(demography_vector, n_susc_groups)
+  demography_vector_spread <- demography_vector_spread * lps
+
+  # replicate contact matrix
+  contact_matrix_spread <- kronecker(
+    X = matrix(1, nrow = n_susc_groups, ncol = n_susc_groups),
+    Y = contact_matrix
+  )
+
+  # multiply rows of expanded contact matrix by susceptibility proportions
+  # and multiply columns by the expanded demography vector
+  contact_matrix_spread <- t(
+    t(contact_matrix_spread * as.vector(susceptibility)) *
+      demography_vector_spread
+  )
 
   # calculate the largest real eigenvalue of the new matrix
   # return the product of the dominant eigenvalue, lambda, and the infectious
   # period as R0
-  max(Re(eigen(cm)$values)) * lambda * infectious_period
+  max(Re(eigen(contact_matrix_spread)$values)) * lambda * infectious_period
 }
 
 #' @title Calculate \eqn{R_0} from transmission rate (\eqn{\lambda})
 #'
-#' @description Uses the transmission rate (\eqn{\lambda}), contact matrix
-#' (\eqn{c}), population (\eqn{N}), and infectious period (\eqn{\gamma}) to
-#' calculate the R0 using the following equation.
-#' \deqn{\lambda max(EV(C)) \gamma}
+#' @description Uses the R0 (\eqn{R0}), contact matrix (\eqn{C}),
+#' population (\eqn{N}), and infectious period (\eqn{\gamma})
+#' to calculate the transmission rate using the following equation.
+#' \deqn{\lambda = R_0 / (\text{Max}(EV(C)) \gamma)}
 #' where \eqn{EV(C)} denotes the eigenvalues of the matrix \eqn{C} which is
-#' calculated from the contact matrix and the population
-#' (\eqn{C[i,j] = c[i,j] N[j]}).
+#' calculated from the social contacts matrix scaled by the number of
+#' individuals in each demographic and susceptibility group in the population.
 #'
 #' @param r0 The basic reproductive number \eqn{R_0} of the infection.
 #' @param contact_matrix Social contact matrix. Entry \eqn{m_{ij}} gives average
 #' number of contacts in group \eqn{i} reported by participants in group \eqn{j}
 #' @param demography_vector Demography vector. Entry \eqn{v_{i}} gives
 #' proportion of total population in group \eqn{i}.
+#' @param susceptibility A matrix giving the susceptibility of individuals in
+#' demographic group \eqn{i} and risk group \eqn{k}.
+#' @param p_susceptibility A matrix giving the probability that an individual
+#' in demography group \eqn{i} is in risk (or susceptibility) group \eqn{k}.
+#' Each row represents the overall distribution of individuals in demographic
+#' group \eqn{i} across risk groups, and each row *must sum to 1.0*.
 #' @param infectious_period Duration of the infectious period in days.
 #' Default value is 1.8 days.
 #' @export
@@ -94,15 +144,32 @@ lambda_to_r0 <- function(lambda, contact_matrix, demography_vector,
 #'
 #' # define R0 similar to pandemic influenza
 #' r0 <- 1.5
-#'
 #' # define infectious period of 5 days
 #' infectious_period <- 5
+#' # define the number of age and susceptibility groups
+#' n_demo_grps <- length(demography_vector)
+#' n_risk_grps <- 3
+#'
+#' # In this example, all risk groups from all age groups are fully
+#' # susceptible
+#' susceptibility <- matrix(
+#'   data = 1, nrow = n_demo_grps, ncol = n_risk_grps
+#' )
+#'
+#' p_susceptibility <- matrix(
+#'   data = 1, nrow = n_demo_grps, ncol = n_risk_grps
+#' )
+#' # p_susceptibility rows must sum to 1.0
+#' p_susceptibility <- p_susceptibility / rowSums(p_susceptibility)
 #'
 #' r0_to_lambda(
-#'   r0, contact_matrix, demography_vector, infectious_period
+#'   r0, contact_matrix, demography_vector,
+#'   susceptibility, p_susceptibility,
+#'   infectious_period
 #' )
 #'
 r0_to_lambda <- function(r0, contact_matrix, demography_vector,
+                         susceptibility, p_susceptibility,
                          infectious_period = 1.8) {
   # input checking
   checkmate::assert_number(r0, lower = 0, finite = TRUE)
@@ -122,12 +189,30 @@ r0_to_lambda <- function(r0, contact_matrix, demography_vector,
       (nrow(contact_matrix) == length(demography_vector))
   )
 
-  # multiply each column of the contact matrix with the corresponding
-  # demography vector element
-  cm <- t(t(contact_matrix) * demography_vector)
+  # count risk groups
+  n_susc_groups <- ncol(p_susceptibility)
+  # make lps, a vector of all p_susc values
+  lps <- as.vector(p_susceptibility)
+
+  # replicate the demography vector and multiply by p_susceptibility
+  demography_vector_spread <- rep(demography_vector, n_susc_groups)
+  demography_vector_spread <- demography_vector_spread * lps
+
+  # replicate contact matrix
+  contact_matrix_spread <- kronecker(
+    X = matrix(1, nrow = n_susc_groups, ncol = n_susc_groups),
+    Y = contact_matrix
+  )
+
+  # divide the contact matrix by the proportion of susceptibles,
+  # and multiply by demography
+  contact_matrix_spread <- t(
+    t(contact_matrix_spread / as.vector(susceptibility)) *
+      demography_vector_spread
+  )
 
   # calculate the largest real eigenvalue of the new matrix
   # return lambda as the R0 divided by the (dominant eigenvalue times the
   # infectious period)
-  r0 / (max(Re(eigen(cm)$values)) * infectious_period)
+  r0 / (max(Re(eigen(contact_matrix_spread)$values)) * infectious_period)
 }
