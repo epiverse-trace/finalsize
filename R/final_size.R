b89a7cf35d5153119370c4c62b0aff2cc07ac3fd
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-05-02 10:54:38 +0100

    Use only.value in eigen() for eigenvalues in final_size
    Co-authored-by: Hugo Gruson <Bisaloo@users.noreply.github.com>

--- R/final_size.R
+++ R/final_size.R
@@ -149,7 +149,7 @@ final_size <- function(r0,
       ),
     "Error: contact matrix must have a maximum real eigenvalue of 1.0" =
       (
-        abs(max(Re(eigen(contact_matrix * demography_vector)$values) - 1.0)) <
+        abs(max(Re(eigen(contact_matrix * demography_vector, only.values = TRUE)$values) - 1.0)) <
           1e-6
       ),
     "Error: control list names can only be: 'iterations', 'tolerance',
